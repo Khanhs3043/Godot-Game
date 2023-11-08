c4ca0772907c61b138c73c47a009b2bc0e1c5010
Trangnguyenthiquynh <124223179+Trangnguyenthiquynh@users.noreply.github.com>
2023-11-08 18:53:19 +0700

    update

--- Global/global.gd
+++ Global/global.gd
@@ -3,7 +3,12 @@ var diary = false
 var diadem = false
 var locket = false
 var ring = false
+<<<<<<< HEAD
 var cup = false
+=======
+var told = false
+var show_win = false
+>>>>>>> parent of 3e73d09 (Revert "Merge branch 'master' of https://github.com/Khanhs3043/Godot-Game")
 var have_diary = false
 
 
